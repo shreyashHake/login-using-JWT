9db2fde06a8918a2f7cf6e9ea0f8860ad4bf5b7d
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-04-24 17:42:19 +0530

    Initialized project

---
+++ jwt-application-backend/src/test/java/com/jwtapp/jwtapplication/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtapp.jwtapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
